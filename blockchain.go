bb93de327407ae43427a1ee9c1b9f07ad02b563f
obynonwane <biostechnologyng@gmail.com>
2024-12-21 01:21:13 +0100

    creating the block and appending to chain

--- blockchain.go
+++ blockchain.go
@@ -25,16 +25,36 @@ func NewBlock(nonce int, previousHash string) *Block {
 }
 
 // helper function to print block
-func(b *Block) Print(){
-	fmt.Printf("timestamp")
+func (b *Block) Print() {
+	fmt.Printf("timestamp            %d\n", b.timestamp)
+	fmt.Printf("nonce                %d\n", b.nonce)
+	fmt.Printf("previous_hash        %s\n", b.previousHash)
+	fmt.Printf("transactions         %s\n", b.transactions)
+}
+
+// creating the blochain
+type Blockchain struct {
+	transactionPool []string
+	chain           []*Block
+}
+
+// create a blockchain by
+// a. creating a new block, using the nonce and previoushash
+// b. appending the new block to the chain
+func (bc *Blockchain) CreateBlock(nonce int, previousHash string) *Block {
+	b := NewBlock(nonce, previousHash)
+	bc.chain = append(bc.chain, b)
+
+	return b
 }
 
 func init() {
 	log.SetPrefix("Blockchain: ")
 }
 func main() {
-	// create a new block
+	// main function initialise and create the first block
+	// with a nonce of 0 and initail hash
 	b := NewBlock(0, "init hash")
+	b.Print()
 
-	fmt.Println(b)
 }
