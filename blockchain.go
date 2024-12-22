57f9afd09037f2d4a0138c11c4faa4178d42433c
obynonwane <biostechnologyng@gmail.com>
2024-12-22 08:48:14 +0100

    update to transaction creation

--- blockchain.go
+++ blockchain.go
@@ -62,13 +62,53 @@ func (bc *Blockchain) CreateBlock(nonce int, previousHash [32]byte) *Block {
 	return b
 }
 
+// return the last block in the chain
+func (bc *Blockchain) LastBlock() *Block {
+	// go into the array and pick the last block
+	// e.g bc.chain[10]
+	return bc.chain[len(bc.chain)-1]
+}
+
 func (bc *Blockchain) Print() {
 	for i, block := range bc.chain {
-		fmt.Printf("%s chain %d %s\n", strings.Repeat("=", 25), i, strings.Repeat("=", 25))
+		fmt.Printf("%s block %d %s\n", strings.Repeat("=", 25), i, strings.Repeat("=", 25))
 		block.Print()
 	}
 
 	fmt.Printf("%s\n", strings.Repeat("*", 25))
+}
+
+// struct for transaction
+type Transaction struct {
+	senderBlockchainAddress    string
+	recipientBlockchainAddress string
+	value                      float32
+}
+
+// function for creating new transaction
+func NewTransaction(sender, recipient string, value float32) *Transaction {
+	return &Transaction{sender, recipient, value}
+}
+
+// print transaction
+func (t *Transaction) Print() {
+	fmt.Printf("%s\n", strings.Repeat("-", 40))
+	fmt.Printf(" senderBlockchainAddress           %s\n", t.senderBlockchainAddress)
+	fmt.Printf(" recipientBlockchainAddress        %s\n", t.recipientBlockchainAddress)
+	fmt.Printf(" value                             %.1f\n", t.value)
+}
+
+// json marshal transaction for hashing
+func (t *Transaction) MarshalJSON() ([]byte, error) {
+	return json.Marshal(struct {
+		SenderBlockchainAddress    string  `json:"sender_blockchain_address"`
+		RecipientBlockchainAddress string  `json:"recipient_blockchain_address"`
+		Value                      float32 `json:"value"`
+	}{
+		SenderBlockchainAddress:    t.recipientBlockchainAddress,
+		RecipientBlockchainAddress: t.recipientBlockchainAddress,
+		Value:                      t.value,
+	})
 }
 
 func (b *Block) Hash() [32]byte {
@@ -101,15 +141,17 @@ func init() {
 	log.SetPrefix("Blockchain: ")
 }
 func main() {
-	block := &Block{nonce: 1}
-	fmt.Printf("%x\n", block.Hash())
+
 	// main function initialise and create the first block
 	// with a nonce of 0 and initail hash
-	// blockChain := NewBlockchain() // init block
+	blockChain := NewBlockchain() // init block
 
 	// subsequent blocks
-	// blockChain.CreateBlock(5, "hash 1")
-	// blockChain.CreateBlock(2, "hash 2")
-	// blockChain.Print()
+	previousHash := blockChain.LastBlock().Hash()
+	blockChain.CreateBlock(1, previousHash)
+
+	previousHash = blockChain.LastBlock().Hash()
+	blockChain.CreateBlock(2, previousHash)
+	blockChain.Print()
 
 }
