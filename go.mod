9fd079172bfe8735cc40743539911eb7fd17316b
obynonwane <biostechnologyng@gmail.com>
2024-12-25 16:45:36 +0100

    update

--- go.mod
+++ go.mod
@@ -1,3 +1,8 @@
 module github.com/obynonwane/blockchain_account_model
 
 go 1.21.0
+
+require (
+	github.com/btcsuite/btcutil v1.0.2
+	golang.org/x/crypto v0.31.0
+)
--- main.go
+++ main.go
@@ -4,30 +4,17 @@ import (
 	"fmt"
 	"log"
 
-	"github.com/obynonwane/blockchain_account_model/block"
+	"github.com/obynonwane/blockchain_account_model/wallet"
 )
 
 func init() {
 	log.SetPrefix("Blockchain: ")
 }
 func main() {
-	myBlockchainAddress := "my_blockchain_address"
-
-	// main function initialise and create the first block
-	// with a nonce of 0 and initail hash
-	blockChain := block.NewBlockchain(myBlockchainAddress) // init block
-
-	// subsequent blocks
-	blockChain.AddTransaction("A", "B", 1.2)
-	blockChain.Mining()
-
-	blockChain.AddTransaction("C", "D", 5.2)
-	blockChain.AddTransaction("E", "G", 10.2)
-	blockChain.Mining()
-	blockChain.Print()
-
-	fmt.Printf("my %.1f\n", blockChain.CalculateTotalAmount("my_blockchain_address"))
-	fmt.Printf("C %.1f\n", blockChain.CalculateTotalAmount("C"))
-	fmt.Printf("D %.1f\n", blockChain.CalculateTotalAmount("D"))
-
+	w := wallet.NewWallet()
+	fmt.Println(w.PrivateKeyStr())
+	fmt.Println(w.PublicKeyStr())
+	fmt.Println(w.BlockchainAddress())
+	t := wallet.NewTrandaction(w.PrivateKey(), w.PublicKey(), w.BlockchainAddress(), "B", 1.0)
+	fmt.Println("signature %s \n", t.GenerateSignature())
 }
