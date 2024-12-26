f54852992f321d8b5d76267bf1b7699eb9fba097
obynonwane <biostechnologyng@gmail.com>
2024-12-26 08:46:03 +0100

    added printing to main function

--- main.go
+++ main.go
@@ -12,17 +12,6 @@ func init() {
 	log.SetPrefix("Blockchain: ")
 }
 func main() {
-	// walletM := wallet.NewWallet()
-	// walletA := wallet.NewWallet()
-	// walletB := wallet.NewWallet()
-
-	// t := wallet.NewTransaction(walletA.PrivateKey(), walletA.PublicKey(), walletA.BlockchainAddress(), walletB.BlockchainAddress(), 1.0)
-	// fmt.Printf("signature %s \n", t.GenerateSignature())
-
-	// //Blockchain Node side
-	// blockchain := block.NewBlockchain(walletM.BlockchainAddress())
-	// isAdded := blockchain.AddTransaction(walletA.BlockchainAddress(), walletB.BlockchainAddress(), 1.0, walletA.PublicKey(), t.GenerateSignature())
-	// fmt.Println("Added?:", isAdded)
 
 	walletM := wallet.NewWallet()
 	walletA := wallet.NewWallet()
@@ -36,4 +25,12 @@ func main() {
 	isAdded := blockchain.AddTransaction(walletA.BlockchainAddress(), walletB.BlockchainAddress(), 1.0,
 		walletA.PublicKey(), t.GenerateSignature())
 	fmt.Println("Added? ", isAdded)
+
+	blockchain.Mining()
+	blockchain.Print()
+
+	fmt.Printf("A %.1f\n", blockchain.CalculateTotalAmount(walletA.BlockchainAddress()))
+	fmt.Printf("B %.1f\n", blockchain.CalculateTotalAmount(walletB.BlockchainAddress()))
+	fmt.Printf("M %.1f\n", blockchain.CalculateTotalAmount(walletM.BlockchainAddress()))
+
 }
